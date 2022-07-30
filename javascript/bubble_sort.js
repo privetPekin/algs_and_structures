922de1c558c87d7166e0319db5cf7707e307b89e
Nikita Vtorushin <n.vtorushin@inbox.ru>
2022-07-30 16:43:16 +0700

    create bubble_sort_js

---
+++ javascript/bubble_sort.js
@@ -0,0 +1,26 @@
+const { randomArray } = require("./utils");
+const array = randomArray(10000, 1000)
+let count = 0
+
+/**
+ * @description - Алгоритм сортировки пузырьком, сложность O(n*n).
+ * @description - В двух циклах проходимся по массиву и сравниваем попарно лежащие элементы, если следующий элемент > предудыщего, то мы меняем их местами
+ * @param {Array} array 
+ */
+function bubbleSort(arr) {
+   let len = arr.length;
+   for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+         if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+         }
+         count += 1
+      }
+   }
+   return arr;
+}
+
+console.log(bubbleSort(array))
+console.log('count:', count)
