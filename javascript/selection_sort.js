187f781f469b42b2f93101c72ec15b719c7e7574
Nikita Vtorushin <n.vtorushin@inbox.ru>
2022-07-30 16:30:33 +0700

    create selection_sort_js

---
+++ javascript/selection_sort.js
@@ -0,0 +1,30 @@
+const { randomArray } = require("./utils");
+const array = randomArray(10000, 1000)
+
+let count = 0
+/**
+ * @description - Алгоритм сортировки выбором, сложность O(n*n).
+ * @description - В цикле находим минимальное число в массиве и меняем его по индексу с первым элементов, далее со вторым и так далее.
+ * @param {Array} array 
+ */
+function selectionSort(arr) {
+   let n = arr.length;
+   for (let i = 0; i < n; i++) {
+      let min = i;
+      for (let j = i + 1; j < n; j++) {
+         if (arr[j] < arr[min]) {
+            min = j;
+         }
+         count += 1
+      }
+      if (min != i) {
+         let tmp = arr[i];
+         arr[i] = arr[min];
+         arr[min] = tmp;
+      }
+   }
+   return arr;
+}
+
+console.log(selectionSort(array))
+console.log('count:', count)
